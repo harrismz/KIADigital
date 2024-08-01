8ecce4afd72f34fbb81835773e8b8123b3e3a82b
Punk73 <teguhsetiawan171@gmail.com>
2024-08-01 19:52:36 +0700

    add image to profileCard

--- resources/js/components/utils/ProfileCard.vue
+++ resources/js/components/utils/ProfileCard.vue
@@ -3,7 +3,12 @@
         <div class="card-body">
             <div class="bg-white p-4 shadow rounded mb-4">
                 <h5 class="text-lg font-bold mb-2">{{ title }}</h5>
-                <p class="mb-4">{{ description }}</p>
+                <div class="grid grid-cols-1 md:grid-cols-4 gap-4">
+                    <p class="mb-4 md:col-span-3">{{ description }}</p>
+                    <div class="flex justify-end md:col-span-1">
+                    <img :src="img" alt="" class="w-24 h-24">
+                    </div>
+                </div>
                 <a :href="link" class="text-blue-500">Selengkapnya ></a>
             </div>
         </div>
@@ -15,7 +20,8 @@ export default {
     props: {
         title: String,
         description: String,
-        link: String
+        link: String,
+        img: String,
     }
 };
 </script>
