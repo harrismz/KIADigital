3b210525feaaba4df17a79bb08cae60e9d25eeb7
Harris <harris.git21@gmail.com>
2024-08-05 10:15:51 +0700

    problem: get user.name basedon login

--- resources/js/components/UserCard.vue
+++ resources/js/components/UserCard.vue
@@ -3,7 +3,13 @@
         <div class="collapse collapse-arrow col-span-1 bg-pink-200 mb-2 shadow-md" :class="{ 'collapse-open': isOpen }">
             <input type="radio" name="my-accordion-2" v-model="isOpen" class="hidden" />
             <div class="collapse-title text-xl font-medium" @click="toggleCollapse">
-                <h1 class="font-mono">{{ user.name }}</h1>
+                <!-- disini problem pengambilan nama user yang login tlg di cek ya guh
+                    dan ketika yg login si ibu ambil nama ibu dari identitas ibu / table mother -->
+                <!-- <h1 class="font-mono">{{ user.name }}</h1> -->
+                <!-- sementara gw comment -->
+                <h1 class="font-mono" v-if="isMedic">MEDIC</h1>
+                <h1 class="font-mono" v-if="isMom">MEDIC</h1>
+                <h1 class="font-mono" v-else>Nama User</h1>
                 <p v-if="isMedic" class="font-sans font-light">
                     NIP : {{ userDetail.nip }}
                 </p>
