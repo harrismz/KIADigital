c08e2c0e1ad193dff95300173e3f7ef2598e8d39
Harris <harris.git21@gmail.com>
2024-08-07 04:31:01 +0700

    feat: Add MyProfile Ibu

--- resources/js/router.js
+++ resources/js/router.js
@@ -33,6 +33,7 @@ import DiaryAnakEdit from './components/anak/ChildWeeklyMonitoringAnswer.vue';
 import PertumbuhanAnak from './components/anak/PertumbuhanAnak.vue';
 import History from './components/medis/history.vue';
 import HistoryDetail from './components/medis/historyDetail.vue';
+import MyProfile from './components/Ibu/MyProfile.vue';
 
 const resolveComponentBasedOnRole = async () => {
     const userRole = store.state.user ? store.state.user.role : null; // Assuming the user's role is stored in the Vuex store
@@ -149,7 +150,16 @@ const routes = [
             requiresAuth: false
         }
     },
-    // Detail Halaman Profile
+    // Detail Halaman Ibu
+    {
+        path: '/my-profile',
+        name: 'my-profile',
+        component: MyProfile,
+        meta: {
+            layout: 'UserLayout',
+            requiresAuth: true
+        }
+    },
     {
         path: '/weekly-monitoring-answer',
         name: 'weekly-monitoring-answer',
