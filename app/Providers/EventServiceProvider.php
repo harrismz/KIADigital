5a591aa901f42bc75e661bbe2c9d5e8b79ebcae0
Punk73 <teguhsetiawan171@gmail.com>
2024-08-16 10:28:04 +0700

    berhasil save ke staff ketika add users

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,10 +2,12 @@
 
 namespace App\Providers;
 
+use App\Listeners\HandleBreadDataAdded;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
+use TCG\Voyager\Events\BreadDataAdded;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -18,6 +20,11 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+
+        BreadDataAdded::class => [
+            HandleBreadDataAdded::class
+        ],
+
     ];
 
     /**
